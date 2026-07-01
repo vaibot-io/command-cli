e264096f896483bbffb5fbf7b68b563c76988be4
Briant Campbell <briantacampbell@gmail.com>
2026-07-01 19:47:09 -0400

    added install script.

---
+++ main/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+  set -eu
+
+  if ! command -v cargo >/dev/null 2>&1; then
+    echo "Installing Rust via rustup..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    export PATH="$HOME/.cargo/bin:$PATH"
+  fi
+
+  cargo install vaibot
+  vaibot init
